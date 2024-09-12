499541c07d73216b4a3cd3ae1720a81597c17e6d
Hiro <84511935+Hiro420@users.noreply.github.com>
2024-09-12 18:22:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ZZZ_TextMap
+ Minimal TextMap parser for anime game with seggsy characters, written in C#
+
+# Usage
+- Compile via Visual Studio 2022
+- Run `ZZZ_TextMap.exe` in CMD, arguments are `<input file path*> <output filename>`
+- The program will create an `output` folder with the textmap json there
+
+\* Input file path must be a valid raw binary file contaning textmap entries\
+\* Output filename will be something like `TextMap_EN.json` or `TextMap_ENTemplateTb.json`
+
+### TextMap_ENTemplateTb will be in `300510925.blk`, with Container path `5902435254996726125` and hashed name `8249065493705925389`
+## I am not going to provide you a blk parser for `mhy1` encrypted blks, only the minimal parser
+
+# I DO NOT CLAIM ANY RESPONSIBILITY FOR ANY USAGE OF THIS SOFTWARE, THE SOFTWARE IS MAKE 100% FOR EDUCATIONAL PURPOSES ONLY
---
+++ ZZZ_TextMap/Program.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+using ZZZ_TextMap.Defs;
+using Google.FlatBuffers;
+
+public static class DictionaryExtensions
+{
+    public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> target, IEnumerable<KeyValuePair<TKey, TValue>> source)
+    {
+        if (target == null)
+            throw new ArgumentNullException(nameof(target));
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+        foreach (var element in source)
+            target.Add(element.Key, element.Value);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2 || !File.Exists(args[0]))
+        {
+            throw new ArgumentException("Please enter a valid location of a binary file (TextMap_ENTemplateTb) and the name of the output JSON file");
+        }
+        string inputDir = args[0];
+        string outputDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!, "output");
+        if (!Directory.Exists(outputDirectory))
+        {
+            Directory.CreateDirectory(outputDirectory);
+        }
+        string outputFilePath = Path.Combine(outputDirectory, args[1]);
+        string textmap_output = ParseTextmapFile(inputDir);
+        File.WriteAllText(outputFilePath, textmap_output);
+        Console.WriteLine("Done!");
+
+    }
+
+    public static string ParseTextmapFile(string filepath)
+    {
+        Dictionary<string, string> result = new Dictionary<string, string>();
+
+        byte[] buffer = [];
+        using (FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+        {
+            buffer = new byte[fs.Length];
+            fs.Read(buffer, 0, buffer.Length);
+        }
+        TextMapTemplateT textmap_template = new();
+        var data = TextMapTemplate.GetRootAsTextMapTemplate(new ByteBuffer(buffer)).UnPack();
+        foreach (var entry in data.Data)
+        {
+            result[entry.Key] = entry.Text;
+        }
+
+        return DataToJson(result);
+    }
+
+    private static string DataToJson<T>(T data)
+    {
+        JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore,
+        };
+
+        return JsonConvert.SerializeObject(data, settings);
+    }
+}
